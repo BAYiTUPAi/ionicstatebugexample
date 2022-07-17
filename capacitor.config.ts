d924b1fb727a60bac6cc36571d38e17ea5e5329d
Jerry <blackshirtsquad@noreply.fake>
2022-07-17 14:41:32 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'StateBugExample',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
